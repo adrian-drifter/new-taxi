d0e711701ccb5e5a9ac5e26778d9a591c89cf51a
adrian acevedo <acidsuper@gmail.com>
2021-12-03 18:06:16 -0600

    all functions added

--- app.py
+++ app.py
@@ -21,13 +21,20 @@ Either as with the title by just creating a string (or an f-string). Or as with 
 - passenger count
 '''
 '''
+
+
 ## Once we have these, let's call our API in order to retrieve a prediction
 
 See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
 
 🤔 How could we call our API ? Off course... The `requests` package 💡
 '''
-
+date = st.text_input('input date and time')
+lon1 = st.text_input('input pickup longitud')
+lat1 = st.text_input('input pickup latitude')
+lon2 = st.text_input('input dropoff longitude')
+lat2 = st.text_input('input dropoff latitude')
+num_pass = st.text_input('input number of passengers')
 url = 'https://taxifare.lewagon.ai/predict?pickup_datetime=2012-10-06%2012:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2'
 
 if url == 'https://taxifare.lewagon.ai/predict':
@@ -45,3 +52,20 @@ if url == 'https://taxifare.lewagon.ai/predict':
 
 ## Finally, we can display the prediction to the user
 '''
+
+
+import requests
+
+
+param = {
+    'pickup_datetime': date,
+    'pickup_longitude': lon1,
+    'pickup_latitude': lat1,
+    'dropoff_longitude': lon2,
+    'dropoff_latitude': lat2,
+    'passenger_count': num_pass
+}
+
+response = requests.get('https://taxifare.lewagon.ai/predict', params=param).json()
+
+st.text(f'the fare will be: {response["prediction"]}')
